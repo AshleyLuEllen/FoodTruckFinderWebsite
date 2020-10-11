20304ac6d615ae43628f6ac64bf74308b530ef1e
Matthew McCaskill <mattrmccaskill@gmail.com>
2020-10-11 02:43:52 -0500

    Main test file

---
+++ food-truck-api/src/test/java/food/truck/api/MainTest.java
@@ -0,0 +1,16 @@
+package food.truck.api;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MainTest {
+    @Test
+    public void passingTest() {
+    }
+
+    @Test
+    public void failingTest() {
+        fail();
+    }
+}
