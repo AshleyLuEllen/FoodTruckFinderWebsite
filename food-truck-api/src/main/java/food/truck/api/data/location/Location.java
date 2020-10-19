ad2645b227a51f88946d732a3a9a014d908a0a74
Matthew McCaskill <mattrmccaskill@gmail.com>
2020-10-19 10:47:47 -0500

    Search and Recommendation endpoints

---
+++ food-truck-api/src/main/java/food/truck/api/data/location/Location.java
@@ -0,0 +1,12 @@
+package food.truck.api.data.location;
+
+import lombok.Data;
+
+@Data
+public class Location {
+    public String name;
+
+    public Double latitude;
+
+    public Double longitude;
+}
