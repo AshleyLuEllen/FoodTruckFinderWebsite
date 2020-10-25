e7d5a46eb2eb7c4f28d03cf1ee41415ccd13a924
brooklynnStone1 <stone.brooklynn@gmail.com>
2020-10-25 14:57:08 -0500

    Converted owner/trucks/#/manage page to use material UI

--- food-truck-frontend/src/pages/owner/trucks/[truck_id]/manage.js
+++ food-truck-frontend/src/pages/owner/trucks/[truck_id]/manage.js
@@ -3,12 +3,14 @@ import Link from "next/link";
 import axios from "axios";
 import { withRouter } from 'next/router';
 import { connect } from "react-redux";
+import {Button, TextField, Container, Grid, InputLabel} from '@material-ui/core';
+import Error from "next/error";
 
 /**
  * Information page for the food trucks which includes an editing form if you're the
  * authenticated owner
  */
-class SearchPage extends Component {
+class ManagePage extends Component {
     constructor(props) {
         super(props);
         this.state = {
@@ -20,10 +22,7 @@ class SearchPage extends Component {
             owner: '',
 
             truckFound: false,
-
-            // Will display the form when true and will display any error messages while editing
-            editing: false,
-            editingMessage: ''
+            cancelDialog: false
         };
 
         this.handleInputChange = this.handleInputChange.bind(this);
@@ -34,9 +33,10 @@ class SearchPage extends Component {
      * Sets the state value to the value in the form
      * @param event the source of the new value in the state
      */
-    handleInputChange(event) {
+    handleInputChange(event, name) {
+        console.log(event.target);
         this.setState({
-            [event.target.name]: event.target.value
+            [name]: event.target.value
         });
     }
 
@@ -44,30 +44,20 @@ class SearchPage extends Component {
         event.preventDefault();
     }
 
-    /**
-     * Checks that the user is authenticated and authorized to edit this
-     * truck's information.
-     */
-    editForm = (() => {
-        axios.get(process.env.FOOD_TRUCK_API_URL + "/users/me",
-            {
-                auth: {
-                    username: this.props.auth.email,
-                    password: this.props.auth.password
-                }
-            }
-        ).then(
+    handleCancel = (() => {
+        this.props.router.push(`/owner/trucks/${this.state.id}`);
+    });
+
+    handleClose = ((option) => {
+        if(option === true) {
+            this.props.router.push(`/owner/trucks/${this.state.id}`);
+        }
+        else {
             this.setState({
-                editing: true,
-                editingMessage: ''
+                cancelDialog: false
             })
-        ).catch((err) => {
-            console.log(err);
-            alert("Only truck owners can edit the truck");
-        });
-
-
-    })
+        }
+    });
 
     /**
      * Saves the edited information from the form
@@ -84,17 +74,11 @@ class SearchPage extends Component {
 
         axios.put(`${process.env.FOOD_TRUCK_API_URL}/trucks/${this.state.id}`, truck)
             .then((res) => {
-                console.log("saved truck!");
-                this.props.router.push(`/owner/trucks/${res.data.id}`).then(r =>
-                    this.setState ( {
-                        editing: false
-                    }));
-
+                this.props.router.push(`/owner/trucks/${res.data.id}`)
+                    .then(r => console.log("Truck Edited"))
             })
             .catch((err) => {
-                this.setState({
-                    editingMessage: err.message
-                });
+                console.log(err.message);
             });
     })
 
@@ -103,6 +87,7 @@ class SearchPage extends Component {
      */
     removeTruck = (() => {
         console.log(this.state);
+        console.log(this.props.auth);
         axios.delete(`${process.env.FOOD_TRUCK_API_URL}/trucks/${this.state.id}`,
             { auth: {
                     username: this.props.auth.email,
@@ -111,14 +96,9 @@ class SearchPage extends Component {
             .then((res) => {
                 console.log(res.statusText);
                 this.props.router.push("/owner/trucks");
-                this.setState({
-                    editing: false
-                });
             })
             .catch((err) => {
-                this.setState({
-                    editingMessage: err.message
-                });
+                console.log(err.message);
             })
     });
 
@@ -136,6 +116,7 @@ class SearchPage extends Component {
                 paymentTypes: res.data.paymentTypes,
                 owner: res.data.owner.id,
                 truckFound: true,
+                cancelDialog: false
             });
             console.log("found the truck!");
         }).catch((err) => {
@@ -147,6 +128,7 @@ class SearchPage extends Component {
                 paymentTypes: 0,
                 owner: 'empty',
                 truckFound: false,
+                cancelDialog: false
             });
         });
     }
@@ -165,7 +147,7 @@ class SearchPage extends Component {
                     paymentTypes: res.data.paymentTypes,
                     owner: res.data.owner,
                     truckFound: true,
-                    editing: false
+                    cancelDialog: false
                 });
                 console.log(this.state);
             }).catch(err => {
@@ -177,7 +159,7 @@ class SearchPage extends Component {
                     paymentTypes: 0,
                     owner: '',
                     truckFound: false,
-                    editing: false
+                    cancelDialog: false
                 });
             });
         }
@@ -186,68 +168,156 @@ class SearchPage extends Component {
     render() {
         return (
             <div>
-                <h2>{this.state.name}</h2>
-                <form onSubmit={this.handleSubmit} method="post">
-                    <span>{this.state.editingMessage}</span>
-                    <table className="truck-form-details">
-                        <tbody>
-                        <tr>
-                            <td>
-                                <label htmlFor="name">
-                                    Name:
-                                </label>
-                            </td>
-                            <td>
-                                <input name="name" type="text" value={this.state.name}
-                                       onChange={this.handleInputChange}/>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <label htmlFor="description">
-                                    Description:
-                                </label>
-                            </td>
-                            <td>
-                                <input name="description" type="text" value={this.state.description}
-                                       onChange={this.handleInputChange}/>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <label htmlFor="licensePlate">
-                                    License Plate:
-                                </label>
-                            </td>
-                            <td>
-                                <input name="licensePlate" type="text" value={this.state.licensePlate}
-                                       onChange={this.handleInputChange}/>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <label htmlFor="paymentTypes">
-                                    Payment Types:
-                                </label>
-                            </td>
-                            <td>
-                                <input name="paymentTypes" type="text" value={this.state.paymentTypes}
-                                       onChange={this.handleInputChange}/>
-                            </td>
-                        </tr>
-                        </tbody>
-                    </table>
-                    <button onClick={this.saveInfo}>Save</button>
-                </form>
-                <button onClick={this.removeTruck}>Delete Truck</button>
-                <br />
-                <br/>
-                <Link href={`/owner/trucks/${this.state.id}`}>
-                    <a>Cancel</a>
-                </Link>
+                <Container>
+                    <h1>{this.state.name}</h1>
+                    <Grid container spacing={4} >
+                        <Grid item xs={12}>
+                            <InputLabel>
+                                Food Truck Name
+                            </InputLabel>
+                            <TextField
+                                id="name"
+                                label=""
+                                value={this.state.name}
+                                fullWidth={true}
+                                onChange={e => this.handleInputChange(e, "name")}
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <InputLabel>
+                                License Plate Number
+                            </InputLabel>
+                            <TextField
+                                id="licensePlate"
+                                label=""
+                                value={this.state.licensePlate}
+                                fullWidth={true}
+                                onChange={e => this.handleInputChange(e, "licensPlate")}
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <InputLabel>
+                                Payment Types
+                            </InputLabel>
+                            <TextField
+                                id="paymentTypes"
+                                label=""
+                                value={this.state.paymentTypes}
+                                fullWidth={true}
+                                onChange={e => this.handleInputChange(e,"paymentTypes")}
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <InputLabel>
+                                Description
+                            </InputLabel>
+                            <TextField
+                                id="description"
+                                label=""
+                                multiline
+                                rows={4}
+                                fullWidth={true}
+                                defaultValue={this.state.description}
+                                onChange={e => this.handleInputChange(e, "description")}
+                            />
+                        </Grid>
+                        <Grid item xs={6} sm={3}>
+                            <Button
+                                variant="outlined"
+                                onClick={this.saveInfo}
+                                width={1/3}>
+                                Save
+                            </Button>
+                        </Grid>
+                        <Grid item xs={6} sm={3}>
+                            <Button
+                                variant="outlined"
+                                onClick={this.removeTruck}
+                                width={1/3}>
+                                Delete
+                            </Button>
+                        </Grid>
+                        <Grid item xs={6} sm={3}>
+                            <Button
+                                variant="outlined"
+                                color="primary"
+                                onClick={this.handleCancel}
+                                href={`/owner/trucks/${this.state.id}`}
+                                width={1/3}>
+                                Cancel
+                            </Button>
+                        </Grid>
+                    </Grid>
+                </Container>
             </div>
         );
     }
+
+    // render() {
+    //     return (
+    //         <div>
+    //             <h2>{this.state.name}</h2>
+    //             <form onSubmit={this.handleSubmit} method="post">
+    //                 <span>{this.state.editingMessage}</span>
+    //                 <table className="truck-form-details">
+    //                     <tbody>
+    //                     <tr>
+    //                         <td>
+    //                             <label htmlFor="name">
+    //                                 Name:
+    //                             </label>
+    //                         </td>
+    //                         <td>
+    //                             <input name="name" type="text" value={this.state.name}
+    //                                    onChange={this.handleInputChange}/>
+    //                         </td>
+    //                     </tr>
+    //                     <tr>
+    //                         <td>
+    //                             <label htmlFor="description">
+    //                                 Description:
+    //                             </label>
+    //                         </td>
+    //                         <td>
+    //                             <input name="description" type="text" value={this.state.description}
+    //                                    onChange={this.handleInputChange}/>
+    //                         </td>
+    //                     </tr>
+    //                     <tr>
+    //                         <td>
+    //                             <label htmlFor="licensePlate">
+    //                                 License Plate:
+    //                             </label>
+    //                         </td>
+    //                         <td>
+    //                             <input name="licensePlate" type="text" value={this.state.licensePlate}
+    //                                    onChange={this.handleInputChange}/>
+    //                         </td>
+    //                     </tr>
+    //                     <tr>
+    //                         <td>
+    //                             <label htmlFor="paymentTypes">
+    //                                 Payment Types:
+    //                             </label>
+    //                         </td>
+    //                         <td>
+    //                             <input name="paymentTypes" type="text" value={this.state.paymentTypes}
+    //                                    onChange={this.handleInputChange}/>
+    //                         </td>
+    //                     </tr>
+    //                     </tbody>
+    //                 </table>
+    //                 <button onClick={this.saveInfo}>Save</button>
+    //             </form>
+    //             <button onClick={this.removeTruck}>Delete Truck</button>
+    //             <br />
+    //             <br/>
+    //             <Link href={`/owner/trucks/${this.state.id}`}>
+    //                 <a>Cancel</a>
+    //             </Link>
+    //         </div>
+    //     );
+    // }
 }
 const mapStateToProps = state => {
     const { auth } = state
@@ -257,4 +327,4 @@ const mapStateToProps = state => {
 const mapDispatchToProps = {
 }
 
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(SearchPage));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(ManagePage));
