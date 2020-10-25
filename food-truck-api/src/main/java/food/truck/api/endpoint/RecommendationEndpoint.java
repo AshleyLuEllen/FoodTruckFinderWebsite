bd6245348c77398c262664df4faf9367516dc196
Matthew McCaskill <mattrmccaskill@gmail.com>
2020-10-25 14:14:51 -0500

    User recommendation endpoint rename

--- food-truck-api/src/main/java/food/truck/api/endpoint/RecommendationEndpoint.java
+++ food-truck-api/src/main/java/food/truck/api/endpoint/RecommendationEndpoint.java
@@ -2,6 +2,7 @@ package food.truck.api.endpoint;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,8 +13,8 @@ import java.security.Principal;
 @Log4j2
 @RestController
 public class RecommendationEndpoint {
-    @GetMapping("/recommendations/me")
-    public void getUserRecommendations(Principal principal) {
-        
+    @GetMapping("/recommendations/{userId}")
+    public void getUserRecommendations(@PathVariable Long userId) {
+
     }
 }
