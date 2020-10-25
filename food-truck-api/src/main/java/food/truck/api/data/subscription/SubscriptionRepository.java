d3d76e9f11fb606d14edc17fe406b5554068ec73
AshleyLuEllen <ashleylu_couch1@baylor.edu>
2020-10-25 13:31:39 -0500

    User Subscription Endpoints Completed.

--- food-truck-api/src/main/java/food/truck/api/data/subscription/SubscriptionRepository.java
+++ food-truck-api/src/main/java/food/truck/api/data/subscription/SubscriptionRepository.java
@@ -1,8 +1,15 @@
 package food.truck.api.data.subscription;
 
+import food.truck.api.data.truck.Truck;
+import food.truck.api.data.truck_tag.TruckTag;
+import food.truck.api.data.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface SubscriptionRepository extends JpaRepository<Subscription, SubscriptionId> {
+        List<Subscription> findByUser(User user);
 }
