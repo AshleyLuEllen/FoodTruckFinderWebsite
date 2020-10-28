288c5cf5127b5c3aa8222d104d70d1957979aa67
Matthew McCaskill <mattrmccaskill@gmail.com>
2020-10-28 13:01:57 -0500

    Tag selector on search page example

--- food-truck-frontend/src/pages/search.js
+++ food-truck-frontend/src/pages/search.js
@@ -1,13 +1,37 @@
-import React from 'react';
-import Link from 'next/link'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+import Link from 'next/link';
+import ChipSelector from '../components/ChipSelector';
 
 function SearchPage() {
+    const [tagOptions, setTagOptions] = useState([]);
+    const [selectedTags, setSelectedTags] = useState([]);
+
+    useEffect(() => {
+        axios.get(`${process.env.FOOD_TRUCK_API_URL}/tags`)
+            .then(res => {
+                setTagOptions(res.data);
+                setSelectedTags(res.data);
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }, []);
+
     return (
         <ul>
             <h2>Search Page</h2>
-            TODO: stub
+            <ChipSelector 
+                label="Tags" 
+                options={tagOptions} 
+                selectedOptions={selectedTags} 
+                onChange={(event, value) => setSelectedTags(value)} 
+                onSelectOption={t => console.log(t)} 
+                onDeselectOption={t => console.log(t)}
+            />
         </ul>
     )
 }
 
-export default SearchPage
+export default SearchPage;
