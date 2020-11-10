c01dc379c0d240c55b3f1520841b18ac368489af
Matthew McCaskill <mattrmccaskill@gmail.com>
2020-11-10 11:53:46 -0600

    Fix current location in the future bug

--- food-truck-api/src/main/java/food/truck/api/data/truck/Truck.java
+++ food-truck-api/src/main/java/food/truck/api/data/truck/Truck.java
@@ -58,7 +58,7 @@ public class Truck {
     @ManyToOne(fetch=FetchType.LAZY)
     @NotFound(action = NotFoundAction.IGNORE)
     @JoinColumnsOrFormulas({
-        @JoinColumnOrFormula(formula=@JoinFormula(value="(SELECT s0.schedule_id FROM " + Schedule.TABLE_NAME + " s0 WHERE s0.truck_id = truck_id AND s0.time_from >= ALL(SELECT s1.time_from FROM " + Schedule.TABLE_NAME + " s1 WHERE s1.truck_id = truck_id) LIMIT 1)", referencedColumnName = "schedule_id"))
+        @JoinColumnOrFormula(formula=@JoinFormula(value="(SELECT s0.schedule_id FROM " + Schedule.TABLE_NAME + " s0 WHERE s0.truck_id = truck_id AND s0.time_from <= CURRENT_TIMESTAMP() AND s0.time_from >= ALL(SELECT s1.time_from FROM " + Schedule.TABLE_NAME + " s1 WHERE s1.truck_id = truck_id AND s1.time_from <= CURRENT_TIMESTAMP()) LIMIT 1)", referencedColumnName = "schedule_id"))
     })
     @Where(clause = "")
     @JsonIgnoreProperties("truck")
