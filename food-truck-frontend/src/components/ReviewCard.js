bcd16698e1e4f58cc227bb808dde77bcfe87bba1
brooklynnStone1 <stone.brooklynn@gmail.com>
2020-11-24 16:15:00 -0600

    Added card content to review card

--- food-truck-frontend/src/components/ReviewCard.js
+++ food-truck-frontend/src/components/ReviewCard.js
@@ -7,6 +7,7 @@ import Rating from "@material-ui/lab/Rating";
 import Divider from "@material-ui/core/Divider";
 import {format} from "date-fns";
 import {makeStyles} from "@material-ui/core/styles";
+import CardContent from "@material-ui/core/CardContent";
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -26,26 +27,27 @@ function ReviewCard(props) {
 
     return (
         <Card className={props.className}>
-            <Typography className={useStyles.root} variant="h8" component="h3" gutterBottom>
-                {format(new Date(props.r.reviewTimestamp), "MM-dd-yyyy, HH:mm")}
-            </Typography>
-            {props.user === false &&
-            <Link href={`/user/${props.r.user.id}`}>
-                <Typography className={useStyles.root} variant="subtitle 1" component="h5" className={useStyles.root} gutterBottom>
-                    By: {props.r.user.firstName} {props.r.user.lastName}
+            <CardContent>
+                <Typography className={useStyles.root} variant="h8" component="h3" gutterBottom>
+                    {format(new Date(props.r.reviewTimestamp), "MM-dd-yyyy, HH:mm")}
                 </Typography>
-            </Link>}
+                {props.user === false &&
+                <Link href={`/user/${props.r.user.id}`}>
+                    <Typography className={useStyles.root} variant="subtitle 1" component="h5" className={useStyles.root} gutterBottom>
+                        By: {props.r.user.firstName} {props.r.user.lastName}
+                    </Typography>
+                </Link>}
 
-            {props.user === true &&
-            <Link href={`/trucks/${props.r.truck.id}`}>
-                <Typography className={useStyles.root} variant="subtitle 1" component="h5" className={useStyles.root} gutterBottom>
-                    For: {props.r.truck.name}
-                </Typography>
-            </Link>}
+                {props.user === true &&
+                <Link href={`/trucks/${props.r.truck.id}`}>
+                    <Typography className={useStyles.root} variant="subtitle 1" component="h5" className={useStyles.root} gutterBottom>
+                        For: {props.r.truck.name}
+                    </Typography>
+                </Link>}
 
-            <Rating className={useStyles.rating} precision={0.5} value={props.r.rating} size="small" readOnly/>
-            {props.r.comment?.split('\n').map(line => <p>{line}</p>)}
-            <Divider/>
+                <Rating className={useStyles.rating} precision={0.5} value={props.r.rating} size="small" readOnly/>
+                {props.r.comment?.split('\n').map(line => <p>{line}</p>)}
+            </CardContent>
         </Card>
     );
 }
