a0d722d2460a1e64d442578e067b2ca18d2f1cb7
Matthew McCaskill <mattrmccaskill@gmail.com>
2020-12-01 19:29:13 -0600

    Fix search/recommendations

--- food-truck-api/src/main/java/food/truck/api/data/schedule/ScheduleRepository.java
+++ food-truck-api/src/main/java/food/truck/api/data/schedule/ScheduleRepository.java
@@ -13,6 +13,6 @@ public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
     List<Schedule> findByTruck(Truck truck);
 
     @Query(value = "SELECT * FROM " + Schedule.TABLE_NAME + " s0 WHERE ( 3959 * acos( cos( radians(?1) ) * cos( radians( truck_latitude ) ) " +
-        "* cos( radians( truck_longitude ) - radians(?2) ) + sin( radians(?1) ) * sin(radians(truck_latitude)))) <= ?3 AND time_from < ?4 AND time_from >= ALL(SELECT time_from FROM " + Schedule.TABLE_NAME + " s1 WHERE s0.truck_id = s1.truck_id)", nativeQuery = true)
+        "* cos( radians( truck_longitude ) - radians(?2) ) + sin( radians(?1) ) * sin(radians(truck_latitude)))) <= ?3 AND time_from < ?4 AND time_from >= ALL(SELECT time_from FROM " + Schedule.TABLE_NAME + " s1 WHERE s0.truck_id = s1.truck_id and s1.truck_id < ?4)", nativeQuery = true)
     List<Schedule> findAllSchedulesNearLocationAfterDate(double latitude, double longitude, double maxDistance, ZonedDateTime date);
 }
